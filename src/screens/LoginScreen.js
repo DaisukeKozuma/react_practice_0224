d39c1e13e63efa16aaba625ae8230d50ba7459b2
Daisuke Kozuma <you@example.com>
2020-02-27 16:33:26 +0900

    first commit

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View, TextInput, Button, Text, TouchableHighlight } from 'react-native';
+
+class LoginScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>helloworld</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+});
+
+
+
+
+
+export default LoginScreen;
